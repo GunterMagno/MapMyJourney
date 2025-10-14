1f03795c6d444414a04d6087059580e6f2c64e0e
GunterMagno <aborcru560@g.educaand.es>
2025-10-14 23:01:21 +0200

    Cambios ci.yaml, aplicado JavaDoc

---
+++ backend/docs/html/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.mapmyjourney.backend"},{"l":"com.mapmyjourney.backend.fileUtils"},{"l":"com.mapmyjourney.backend.model"}];updateSearchResults();
