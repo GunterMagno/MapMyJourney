849d35e41acd39c3a4dd8b6eaeb17743ad7513de
GunterMagno <aborcru560@g.educaand.es>
2026-02-10 11:15:35 +0100

    Empezado a crear componente de paigna, y enrutado (pero por alguna razon no funcionan los botones del home para registrarse ni las paginas ni nada)

---
+++ frontend/src/app/features/trips/pages/trip-notes/trip-notes.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+/*import { HeaderComponent } from '../../layout/header/header';
+import { FooterComponent } from '../../layout/footer/footer';*/
+
+@Component({
+  selector: 'app-trip-notes',
+  imports: [/*HeaderComponent, FooterComponent*/],
+  templateUrl: './trip-notes.component.html',
+  styleUrl: './trip-notes.component.scss',
+})
+export class NotesComponent {
+  
+}
