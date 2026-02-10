435bed02db22a93e43bfc9abd65dcedac3aa529a
GunterMagno <aborcru560@g.educaand.es>
2026-02-10 11:58:21 +0100

    Commit reglamentario

--- frontend/src/app/features/trips/pages/trip-notes/trip-notes.component.ts
+++ frontend/src/app/features/trips/pages/trip-notes/trip-notes.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-/*import { HeaderComponent } from '../../layout/header/header';
-import { FooterComponent } from '../../layout/footer/footer';*/
 
 @Component({
   selector: 'app-trip-notes',
-  imports: [/*HeaderComponent, FooterComponent*/],
+  imports: [
+    ],
   templateUrl: './trip-notes.component.html',
   styleUrl: './trip-notes.component.scss',
 })
 export class NotesComponent {
+ 
   
+  private loadNotes(): void{
+    this.
+  }
+
+
 }
